76465c6b022b0f3cced4ee61a0e85b631448df91
Kendall Strautman <kendallstraut@gmail.com>
2019-08-02 12:50:50 -0700

    added export script

---
+++ src/.next/server/6.b8400e1b6f55098e2163.js
@@ -0,0 +1,13 @@
+exports.ids = [6];
+exports.modules = {
+
+/***/ "/DR1":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ("---\ntitle: The coastal red giants\nauthor: Ricardo Nagaoka\ndate: 2019-07-10T16:04:44.000+00:00\nhero_image: \"static/jj-ying-7JX0-bfiuxQ-unsplash.jpg\"\n---\n\nPseudotsuga menziesii is an evergreen conifer species in the pine family, Pinaceae. It is native to western North America and is known as Douglas fir,Douglas-fir,Oregon pine,and Columbian pine. There are two varieties: coast Douglas-fir (P. menziesii var. menziesii), and Rocky Mountain Douglas-fir (P. menziesii var. glauca).\n\nChicharrones kinfolk tumblr narwhal venmo. Iceland cloud bread 8-bit, hashtag single-origin coffee keffiyeh drinking vinegar cray banjo four dollar toast bespoke tbh live-edge celiac. YOLO dreamcatcher hot chicken selvage portland XOXO, single-origin coffee sustainable mustache thundercats fixie pour-over hexagon irony deep v. Farm-to-table ennui mixtape marfa raw denim austin hell of keffiyeh chia prism ethical. Polaroid biodiesel everyday carry VHS hot chicken chartreuse typewriter messenger bag. Cloud bread truffaut street art jean shorts chia.\n");
+
+/***/ })
+
+};;
