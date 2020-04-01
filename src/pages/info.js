b216cf4cfdfd8605db248641af2ebb7b488882ce
Kendall Strautman <kendallstrautman@gmail.com>
2020-04-01 08:52:17 -0600

    chore: update info page --> getStaticProps

--- src/pages/info.js
+++ src/pages/info.js
@@ -1,45 +1,49 @@
-import Layout from "../components/Layout";
-import matter from "gray-matter";
-import ReactMarkdown from "react-markdown";
+import Layout from '../components/Layout'
+import matter from 'gray-matter'
+import ReactMarkdown from 'react-markdown'
 
-export default function Info(props) {
-  const frontmatter = props.data
-  const markdownBody = props.content
-  return (
-    <Layout pathname='info' bgColor={frontmatter.background_color} siteTitle={props.title}>
-    <section className="info_blurb">
-      <ReactMarkdown source={markdownBody} />
-    </section>
-    <style jsx>{`
-      .info_blurb {
-        max-width: 800px;
-        padding: 1.5rem 1.25rem;
-      }
-      
-      @media (min-width: 768px) {
-        .info_blurb {
-          padding: 2rem;
-        }
-      }
-      
-      @media (min-width: 1440px) {
-        .info_blurb {
-          padding: 3rem;
-        }
-      }
-    `}</style>
-  </Layout>
-  );
+export default function Info({ frontmatter, markdownBody, title }) {
+	return (
+		<Layout
+			pathname='info'
+			bgColor={frontmatter.background_color}
+			siteTitle={title}
+		>
+			<section className='info_blurb'>
+				<ReactMarkdown source={markdownBody} />
+			</section>
+			<style jsx>{`
+				.info_blurb {
+					max-width: 800px;
+					padding: 1.5rem 1.25rem;
+				}
+
+				@media (min-width: 768px) {
+					.info_blurb {
+						padding: 2rem;
+					}
+				}
+
+				@media (min-width: 1440px) {
+					.info_blurb {
+						padding: 3rem;
+					}
+				}
+			`}</style>
+		</Layout>
+	)
 }
 
+export async function getStaticProps() {
+	const content = await import(`../data/info.md`)
+	const config = await import(`../data/config.json`)
+	const data = matter(content.default)
 
-Info.getInitialProps = async function() {
-  const content = await import(`../data/info.md`)
-  const config = await import(`../data/config.json`)
-  const data = matter(content.default)
-
-  return {
-    title: config.title,
-    ...data
-  }
+	return {
+		props: {
+			title: config.title,
+			frontmatter: data.data,
+			markdownBody: data.content
+		}
+	}
 }
