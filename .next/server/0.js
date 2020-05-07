55a07baef6b5ff087d2c351472b7df5b07ef7191
Kendall Strautman <kendallstrautman@gmail.com>
2020-05-07 12:21:12 -0700

    refactor: remove src subdir

---
+++ .next/server/0.js
@@ -0,0 +1,16 @@
+exports.ids = [0];
+exports.modules = {
+
+/***/ "./data/config.json":
+/*!**************************!*\
+  !*** ./data/config.json ***!
+  \**************************/
+/*! exports provided: title, description, repositoryUrl, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"title\":\"Brevifolia\",\"description\":\"Starter blog using Tina CMS with Next.js.\",\"repositoryUrl\":\"https://github.com/kendallstrautman/brevifolia-next-tinacms\"}");
+
+/***/ })
+
+};;
+//# sourceMappingURL=0.js.map
